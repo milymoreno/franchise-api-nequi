910e5a55354cdc8b5d84f4de9d534ec7e60a41eb
milymoreno <milymoreno@users.noreply.github.com>
2026-02-23 12:58:16 -0500

    fix: add OpenAPI security definitions for JWT Bearer auth in Swagger UI

--- src/main/java/com/nequi/franchises/FranchiseApiApplication.java
+++ src/main/java/com/nequi/franchises/FranchiseApiApplication.java
@@ -1,9 +1,16 @@
 package com.nequi.franchises;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Franchise API", version = "1.0", description = "NEQUI Technical Test"), security = @SecurityRequirement(name = "BearerAuth"))
+@SecurityScheme(name = "BearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
 public class FranchiseApiApplication {
 
 	public static void main(String[] args) {
